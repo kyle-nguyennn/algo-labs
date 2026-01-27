21b558ed179eecdc59c760f177db3f43c728f029
Kyle Nguyen <nghia002@outlook.com>
2026-01-27 23:26:31 +0800

    HW2: boilerplate md

--- hw0/cs6515_hw0.py
+++ hw0/cs6515_hw0.py
@@ -1,2 +1,28 @@
 def HW0(A: tuple[int, ...]) -> tuple[int, list[int]]:
-    pass
+    n = len(A)
+
+    if n == 0:
+        return 0, []
+
+    t = []
+
+    for i in range(n):
+        t.append(1)
+
+        for j in range(i):
+            if (A[j] < A[i]) and (t[i] < 1 + t[j]):
+                t[i] = 1 + t[j]
+
+    length = end = max(t)
+
+    positions = []
+
+    for i in reversed(range(n)):
+        if t[i] == end:
+            positions.append(i)
+
+            end -= 1
+
+    positions.reverse()
+
+    return length, positions
