ccd582d3839e98f714e128507f56017abb9c72e9
Kyle Nguyen <nghia002@outlook.com>
2026-02-11 23:38:43 +0800

    HW3: optimize for loop

--- hw3/cs6515_woot_off.py
+++ hw3/cs6515_woot_off.py
@@ -3,12 +3,15 @@ def WootOff(
 ) -> tuple[int, list[int]]:
     max_cap = max(C)
     n = len(W)
+    wp = zip(W, P)
+    wp = sorted(wp)
     # knapsack on max_cap
     dp = [0] * (max_cap+1) # max profit when cap = i
     for cap in range(max_cap+1):
-        for item in range(n):
-            if cap >= W[item]:
-                dp[cap] = max(dp[cap], dp[cap-W[item]]+P[item])
+        for weight, profit in wp:
+            if weight > cap:
+                break
+            dp[cap] = max(dp[cap], dp[cap-weight]+profit)
     # print(dp)
     # extract result for each city
     res = [dp[city_cap] for city_cap in C]
