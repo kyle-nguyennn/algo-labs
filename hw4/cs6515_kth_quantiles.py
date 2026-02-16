850855019a3bbe46019fa13f6e3e0daf8faee595
Kyle Nguyen <nghia002@outlook.com>
2026-02-16 20:05:52 +0700

    HW4: O(n) optimization

--- hw4/cs6515_kth_quantiles.py
+++ hw4/cs6515_kth_quantiles.py
@@ -1,43 +1,61 @@
+import bisect
+
 def swap(S, i, j):
     S[i], S[j] = S[j], S[i]
-
-def kth_select(S, k):
-    n = len(S)
-    if n < 5:
-        S = sorted(S)
-        return S[(len(S)-1)//2] # left median
-
-    p = n//5
-    medians = []
-    for i in range(0, n, p):
-        chunk =  S[i: min(i+p, len(S))]
-        medians.append(kth_select(chunk, len(chunk)//2))
-    medians = sorted(medians)
-    pivot = medians[2]
-    # TODO: to be optimized by swapping
-    left = [e for e in S if e<pivot]
-    middle = [e for e in S if e==pivot]
-    right = [e for e in S if e>pivot]
-    if k < len(left): return kth_select(left, k)
-    if len(left) <= k < len(left) + len(middle): return pivot
-    else: return kth_select(right, k - len(left) - len(middle))
 
 def KthQuantiles(S: list[int], k: int) -> list[int]:
     # −1, 2, 4, 1, 3, 0, 18, −3
     # -3, -1, 0, 1, 2, 3, 4, 18
     # k = 4: -1 1 3
     # S = sorted(S)
-    if k==1: return []
     n = len(S)
+    if k==1 or n<k: return []
     p = n//k
     res = []
     pivots = list(range(p-1, n-1, p))
-    median = kth_select(S, n//2) # true median, unique number
-    # TODO: to be optimized by swapping
-    left = [e for e in S if e<=median]
-    right = [e for e in S if e>median]
-    # find the kth_quantiles covered by left 
-    left_quantiles = KthQuantiles(left, k//2)
-    right_quantiles = KthQuantiles(right, k//2)
+    # estimate median
+    if n <= 4:
+        S = sorted(S)
+        return [S[i] for i in pivots] # left median
 
-    return left_quantiles + [median] + right_quantiles
+    p = n//4
+    medians = []
+    for i in range(0, n, p):
+        chunk =  S[i: min(i+p, len(S))]
+        if len(chunk) == 1:
+            medians.append(chunk[0])
+        else:
+            medians.append(KthQuantiles(chunk, 2)[0])
+    medians = sorted(medians)
+    pivot = medians[1]
+    ### estimate median end
+    # TODO: to be optimized by swapping
+    left = [e for e in S if e<=pivot]
+    right = [e for e in S if e>pivot]
+    # print(f"pivot: {pivot}")
+    # print(f"left: {left}")
+    # print(f"right: {right}")
+    # find the kth_quantiles covered by left 
+    left_quantile_idx = bisect.bisect_left(pivots, len(left))
+    # print(f"pivots: {pivots}")
+    # print(f"left_quantile_idx: {left_quantile_idx}")
+    middle_quantile = []
+    if len(left) < pivots[left_quantile_idx]+1:
+        fill = pivots[left_quantile_idx] + 1 - len(left)
+        left = left + [float("inf")]*fill
+        right =  [float("-inf")]*(p - fill) + right
+    elif len(left) == pivots[left_quantile_idx]+1:
+        middle_quantile = [pivot]
+    # print(f"left: {left}")
+    # print(f"right: {right}")
+    
+    left_quantiles = KthQuantiles(left, left_quantile_idx+1)
+    right_quantiles = KthQuantiles(right, k-left_quantile_idx - len(middle_quantile))
+    # print(f"left quantiles: {left_quantiles}")
+    # print(f"right quantiles: {right_quantiles}")
+
+    return left_quantiles + middle_quantile + right_quantiles
+
+if __name__=="__main__":
+    quantiles = KthQuantiles([−1, 2, 4, 1, 3, 0, 18, −3],4)
+    assert(quantiles == [-1, 1, 3])
