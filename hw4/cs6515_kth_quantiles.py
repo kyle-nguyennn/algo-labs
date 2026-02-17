f2f91d3c9ee993e7ea0f664f7fabea70cff37565
Kyle Nguyen <nghia002@outlook.com>
2026-02-17 17:35:59 +0700

    HW4: self implement bisect_left

--- hw4/cs6515_kth_quantiles.py
+++ hw4/cs6515_kth_quantiles.py
@@ -1,7 +1,17 @@
-import bisect
-
 def swap(S, i, j):
     S[i], S[j] = S[j], S[i]
+
+def binary_search(a, e):
+    l = 0
+    r = len(a)
+    # l, r is half open interval [l,r)
+    while l < r:
+        mid = (l + r)//2
+        if a[mid] < e:
+            l = mid + 1
+        else:
+            r = mid
+    return l
 
 def KthQuantiles(S: list[int], k: int) -> list[int]:
     # −1, 2, 4, 1, 3, 0, 18, −3
@@ -36,7 +46,7 @@ def KthQuantiles(S: list[int], k: int) -> list[int]:
     # print(f"left: {left}")
     # print(f"right: {right}")
     # find the kth_quantiles covered by left 
-    left_quantile_idx = bisect.bisect_left(pivots, len(left))
+    left_quantile_idx = binary_search(pivots, len(left))
     # print(f"pivots: {pivots}")
     # print(f"left_quantile_idx: {left_quantile_idx}")
     middle_quantile = []
@@ -56,6 +66,6 @@ def KthQuantiles(S: list[int], k: int) -> list[int]:
 
     return left_quantiles + middle_quantile + right_quantiles
 
-if __name__=="__main__":
-    quantiles = KthQuantiles([−1, 2, 4, 1, 3, 0, 18, −3],4)
-    assert(quantiles == [-1, 1, 3])
+# if __name__=="__main__":
+#     quantiles = KthQuantiles([−1, 2, 4, 1, 3, 0, 18, −3],4)
+#     assert(quantiles == [-1, 1, 3])
