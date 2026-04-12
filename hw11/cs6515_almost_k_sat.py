78d003b2d80e216563e9827e703b66577b62bbea
Kyle Nguyen <nghia002@outlook.com>
2026-04-12 14:34:20 +0800

    HW11: boilerplate

---
+++ hw11/cs6515_almost_k_sat.py
@@ -0,0 +1,13 @@
+from npc.types import CNF, Assignment, Clause
+
+
+def verify_solution(cnf: CNF, variables: Assignment, K: int) -> bool:
+    pass
+
+
+def input_transformation(cnf: CNF) -> tuple[CNF, int]:
+    pass
+
+
+def output_transformation(variables: Assignment | None) -> Assignment | None:
+    pass
