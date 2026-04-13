19fa93e255ecf81e43e9a06f3bdcb724879479bb
Kyle Nguyen <nghia002@outlook.com>
2026-04-13 09:00:46 +0000

    HW11: remove stdout

--- hw11/cs6515_almost_k_sat.py
+++ hw11/cs6515_almost_k_sat.py
@@ -22,11 +22,11 @@ def _evaluate_clause(clause: Clause, values: dict[str, bool]) -> bool:
 
 def verify_solution(cnf: CNF, variables: Assignment, K: int) -> bool:
     cnt = 0
-    print(f"Verifying assignment: {variables}")
+    # print(f"Verifying assignment: {variables}")
     values = _evaluate_assignment(variables)
     for clause in cnf:
         if not (val := _evaluate_clause(clause, values)):
-            print(f"{clause} is unsatisfied")
+            # print(f"{clause} is unsatisfied")
             cnt += 1
             if cnt > K: return False
     return cnt == K
