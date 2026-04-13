6cb8e713250256e199becc4512151340dc82ae8e
Kyle Nguyen <nghia002@outlook.com>
2026-04-13 16:35:56 +0800

    [HW11] Verify solutions

--- hw11/cs6515_almost_k_sat.py
+++ hw11/cs6515_almost_k_sat.py
@@ -1,8 +1,33 @@
 from npc.types import CNF, Assignment, Clause
 
+def _evaluate_assignment(variables: Assignment) -> dict[str, bool]:
+    values = {}
+    for v in variables:
+        if v.startsWith('!'):
+            values[v] = False
+        else:
+            values[v] = True
+    return values
+
+def _evaluate_clause(clause: Clause, values: dict[str, bool]) -> bool:
+    res = False
+    for v in clause:
+        if v.startsWith('!'):
+            val = not values[v]
+        else:
+            val = values[v]
+        res = res or val
+        if res == True: return True
+    return res
 
 def verify_solution(cnf: CNF, variables: Assignment, K: int) -> bool:
-    pass
+    cnt = 0
+    values = _evaluate_assignment(variables)
+    for clause in cnf:
+        if _evaluate_clause(clause, values):
+            cnt += 1
+            if cnt > K: return False
+    return cnt == K
 
 
 def input_transformation(cnf: CNF) -> tuple[CNF, int]:
